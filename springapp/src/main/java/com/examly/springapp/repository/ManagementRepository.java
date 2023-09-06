cbe4e9cee542b490436e53579a21030a6ad74c75
Iamneo <techadmin@iamneo.ai>
2023-09-06 15:51:04 +0000

    Backup Commit

--- springapp/src/main/java/com/examly/springapp/repository/ManagementRepository.java
+++ springapp/src/main/java/com/examly/springapp/repository/ManagementRepository.java
@@ -1,26 +1,13 @@
-package com.example.TaskManagement.repository;
+package com.examly.springapp.repository;
 
-import java.util.List;
-
+import com.examly.springapp.entity.Taskentity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
-import com.example.TaskManagement.entity.Management;
-
-import jakarta.transaction.Transactional;
-
-
+import java.util.Optional;
+ 
 @Repository
-public interface ManagementRepository extends JpaRepository<Management, Integer>{
-	
-	
-	@Transactional
-    @Modifying
-    @Query(value="update Management m set m.taskstatus = ?1 where m.taskid = ?2",nativeQuery=true)
-    void updateTask(String status,int id);
-	
-	List<Management> findBytaskNameLike(String taskName);
+public interface TaskRepository  extends JpaRepository<Taskentity,String>{
+Optional<Taskentity> findByTaskId(String taskid);
+void deleteByTaskId(String taskid);
 
 }
