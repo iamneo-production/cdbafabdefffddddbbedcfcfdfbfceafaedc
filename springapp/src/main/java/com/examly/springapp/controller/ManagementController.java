182b21ef78ae2466075872877fdb13867f19e989
Iamneo <techadmin@iamneo.ai>
2023-09-06 15:56:03 +0000

    Backup Commit

--- springapp/src/main/java/com/examly/springapp/controller/ManagementController.java
+++ springapp/src/main/java/com/examly/springapp/controller/ManagementController.java
@@ -1,72 +1,47 @@
-package com.example.TaskManagement.controller;
+package com.examly.springapp.controller;
 
+import com.examly.springapp.entity.Taskentity;
+import com.examly.springapp.service.TaskService;
 import java.util.List;
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-import com.example.TaskManagement.entity.Management;
-import com.example.TaskManagement.repository.ManagementRepository;
+@RestController
+@RequestMapping()
+public class TaskController {
+    @Autowired
+    private TaskService service;
+    
+    @PostMapping("/saveTask")
+    public Taskentity saveTask(@RequestBody Taskentity taskentity){
+        Taskentity output = service.saveTask(taskentity);
+        return output;
+    }
 
-@Controller
-public class ManagementController {
-	
-	@Autowired
-	private ManagementRepository repo;
-	
-	@RequestMapping("/")
-	@ResponseBody
-	public String home(@RequestParam int taskid,String name,String date,String taskname,String progress)
-	{
-		
-		Management manage=new Management();
-		manage.setTaskid(taskid);
-		manage.setTaskHolderName(name);
-		manage.setTaskDate(date);
-		manage.setTaskName(taskname);
-		manage.setTaskstatus(progress);
-		repo.save(manage);
-		return "Welcome to Task Management!";
-	}
-	
-	@RequestMapping("/saveTask") 
-	@ResponseBody
-	public List<Management> saveTask()
-	{
-		return repo.findAll();
-	}
-	
-	@RequestMapping("/changeStatus")
-	@ResponseBody
-	public Optional<Management> updateTask(@RequestParam int id)
-	{
-		String status="Completed";
-		repo.updateTask(status, id);
-		Optional<Management> manage1=repo.findById(id);
-		return manage1;
-		 
-	}
-	
-	@RequestMapping("/deleteTask")
-	@ResponseBody
-	public String DeleteTask(@RequestParam int id)
-	{
-		repo.deleteById(id);
-		return "Delete Sucessful!";
-	}
-	
-	@RequestMapping("/getTask")
-	@ResponseBody
-	public ResponseEntity<List<Management>> getProductsByType(@RequestParam String taskName) {
-			return new ResponseEntity<List<Management>>(repo.findBytaskNameLike("%"+taskName+"%"), HttpStatus.OK);
-		}
-	
+
+    @GetMapping("/changeStatus")
+    private Taskentity updatetaskStatus(@RequestParam("id") String id){
+        return service.updatetaskStatus(id);
+    }
+
+    @GetMapping("/deleteTask")
+    private String deleteTask(@RequestParam("id") String id){
+        return service.deleteTask(id);
+    }
+    @GetMapping("/alltasks")
+    private List<Taskentity> getallTasks(){
+        return service.getallTasks();
+    }
+
+    @GetMapping("/getTask")
+    private Taskentity getTask(@RequestParam("id") String id){
+        return service.gettaskbyid(id);
+    }
 
 }
--- springapp/src/main/java/com/examly/springapp/entity/Management.java
+++ springapp/src/main/java/com/examly/springapp/entity/Management.java
@@ -1,77 +1,58 @@
-package com.example.TaskManagement.entity;
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.SequenceGenerator;
-import jakarta.persistence.Table;
-import jakarta.persistence.UniqueConstraint;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
-@Table( name="management",
-schema="librarymanagement",
-uniqueConstraints= {
-		@UniqueConstraint(
-				name= "taskHolderName_unique",
-				columnNames="taskHolderName"
-				)
-        }
-		)
-public class Management {
-	
-  
-  @Id
-  @GeneratedValue(
-		  
-		  strategy = GenerationType.SEQUENCE,
-			 generator = "management_generator"
-		  )
-  @SequenceGenerator(
-			name="management_generator",
-			sequenceName = "management_generator_name"
-			)
-  private int taskid;
-  
-  private String taskHolderName;
-  private String taskDate;
-  private String taskName;
-  private String taskstatus;
-public int getTaskid() {
-	return taskid;
-}
-public void setTaskid(int taskid) {
-	this.taskid = taskid;
-}
-public String getTaskHolderName() {
-	return taskHolderName;
-}
-public void setTaskHolderName(String taskHolderName) {
-	this.taskHolderName = taskHolderName;
-}
-public String getTaskDate() {
-	return taskDate;
-}
-public void setTaskDate(String taskDate) {
-	this.taskDate = taskDate;
-}
-public String getTaskName() {
-	return taskName;
-}
-public void setTaskName(String taskName) {
-	this.taskName = taskName;
-}
-public String getTaskstatus() {
-	return taskstatus;
-}
-public void setTaskstatus(String taskstatus) {
-	this.taskstatus = taskstatus;
-}
-  
-  
-  
+@Table(name="Task")
+public class Taskentity {
 
+    @Id
+    private String taskId;
+    private String taskHolderName;
+    private String taskDate;
+    private String taskName;
+    private String taskStatus;
+   
+
+    public String getTaskId() {
+        return taskId;
+    }
+    public void setTaskId(String taskId) {
+        this.taskId = taskId;
+    }
+    public String getTaskHolderName() {
+        return taskHolderName;
+    }
+    public void setTaskHolderName(String taskHolderName) {
+        this.taskHolderName = taskHolderName;
+    }
+    public String getTaskDate() {
+        return taskDate;
+    }
+    public void setTaskDate(String taskDate) {
+        this.taskDate = taskDate;
+    }
+    public String getTaskName() {
+        return taskName;
+    }
+    public void setTaskName(String taskName) {
+        this.taskName = taskName;
+    }
+    public String getTaskStatus(){
+        return taskStatus;
+    }
+    public void setTaskStatus(String taskStatus){
+        this.taskStatus = taskStatus;
+    }
+
+    public Taskentity(String taskId, String taskHolderName, String taskDate, String taskName, String taskStatus) {
+        this.taskId = taskId;
+        this.taskHolderName = taskHolderName;
+        this.taskDate = taskDate;
+        this.taskName = taskName;
+        this.taskStatus = taskStatus;
+    }
+  public Taskentity(){
+      
+  }  
 }
-
-
-
